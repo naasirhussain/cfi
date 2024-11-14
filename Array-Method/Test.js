e950bdc8e5df23034503d873d1475b4cfab80bba
naasirhussain <naasirhussain2002@mail.com>
2024-11-14 04:09:20 +0000

    array mthds

--- Array-Method/Test.js
+++
@@ -1,6 +0,0 @@
-function evenOdd (x) {
-    if (x % 4){
-        console.log("this is even");}
- else{
-        console.log("this is odd");
- } 
--- Array-Method/pop & shift.js
+++
@@ -1,7 +0,0 @@
-// Removing elements
-
-// pop:- Removes the last element from an array and returns it.
-// shift :- Removes thr first element from an array and returns it
-
-
-// pop
